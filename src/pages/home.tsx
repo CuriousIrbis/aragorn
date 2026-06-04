f8b4a782a47364abbf29ae75cfb2afc4991777c9
Mark <mega.nemirovskiy@gmail.com>
2026-06-04 13:23:22 +0300

    Разбирался с axios. Изменил доступ к интересам на прямой

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,10 @@
 import { useLocation } from 'react-router';
 import '/src/style/layout/home.scss'
-import axios from 'axios';
-import { useEffect } from 'react';
-
-async function getFavorites(){
-    try{
-        const rawData = await axios.get('/src/assets/text-data/favorites.json')
-        const data = JSON.stringify(rawData.data)
-        console.log(data)
-    } catch(error){
-        console.error(error)
-    }
-}
 
 export default function Home(){
     const location = useLocation();
     const {title} = location.state || {};
 
-    useEffect(() => {
-        getFavorites()
-    }, [])
 
     return(
         <section className="home">
--- src/pages/interests.tsx
+++ src/pages/interests.tsx
@@ -3,30 +3,16 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import Knowledge from "../components/service/knowledge";
 
+import interests from '../assets/text-data/favorites.json'
+
 interface Interes{
     title: string
     description: string
 }
 
-async function getInterests(){
-    try{
-        const interests = await axios.get('/src/assets/text-data/favorites.json')
-        return interests.data;
-    } catch(error: AxiosError | any){
-        console.error(error);
-        throw new Error("Ошибка при открытии файла");
-    }
-}
-
 export default function Interests(){
     const location = useLocation();
     const {title} = location.state || {};
-
-    const [interests, setinterests] = useState(Array<Interes | Error>)
-
-    useEffect(() => {
-        getInterests().then((value: Array<Interes | Error>) => setinterests(value))
-    })
 
     return (
         <section>
