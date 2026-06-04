81648eb07be621e26c548fc8e7bb56f90415da3b
Mark <mega.nemirovskiy@gmail.com>
2026-06-04 13:24:37 +0300

    Продолжаю бороться с Vercel

--- src/pages/interests.tsx
+++ src/pages/interests.tsx
@@ -1,14 +1,7 @@
-import axios, { AxiosError } from "axios";
-import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import Knowledge from "../components/service/knowledge";
 
 import interests from '../assets/text-data/favorites.json'
-
-interface Interes{
-    title: string
-    description: string
-}
 
 export default function Interests(){
     const location = useLocation();
